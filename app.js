204c3f6cb17c2382e7349d1df6350cba7dc8afed
KreiZi0 <steven.mirontsuk1@gmail.com>
2022-01-10 12:57:26 +0200

    Added item to data structure

---
+++ app.js
@@ -0,0 +1,121 @@
+// Storage Controller
+// create later
+
+// Item Controller
+const ItemCtrl = (function(){
+// Item Constructor
+const Item = function(id, name, calories){
+this.id = id
+this.name = name
+this.calories = calories
+}
+
+// Data Structure
+const data = {
+items: [
+{id: 0, name: 'Steak Dinner', calories: 1200},
+{id: 1, name: 'Cookie', calories: 400},
+{id: 2, name: 'Eggs', calories: 300}
+],
+total: 0
+}
+
+
+return {
+getItems: function(){
+return data.items
+},
+addItem: function(name,calories){
+	let ID;
+	if(data.items.length > 0){
+		ID=data.items[data.items.length - 1].id + 1
+		
+	} else {
+		ID = 0
+	}
+	calories = parseInt(calories);
+	newItem = new Item(ID, name, calories);
+	data.items.push(newItem);
+	return newItem
+},
+logData: function(){
+return data
+}
+}
+})();
+
+
+// UI Controller
+const UICtrl = (function(){
+	//UI Selector 
+	const UISelectors = {
+		itemNameInput: '#item-name',
+		itemCaloriesInput: '#item-calories',
+		addBtn: '.add-btn'
+	}
+return {
+populateItemList: function(items){
+// create html content
+let html = '';
+
+// parse data and create list items html
+items.forEach(function(item){
+html += `<li class="collection-item" id="item-${item.id}">
+<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+<a href="#" class="secondary-content">
+<i class="edit item fa fa-pencil"></i>
+</a>
+</li>`;
+});
+
+// insert list items
+document.querySelector("#item-list").innerHTML = html;
+},
+
+getSelectors: function(){
+	return UISelectors;
+},
+getItemInput:function(){
+	return {
+		name:document.querySelector(UISelectors.itemNameInput).value,calories:document.querySelector(UISelectors.itemCaloriesInput).value
+
+	}
+}
+
+}
+})();
+
+
+// App Controller
+const App = (function(ItemCtrl, UICtrl){
+	//load event listeners
+	const loadEventListeners = function(){
+		//get UI selectors
+		const UISelectors = UICtrl.getSelectors()
+		// add item event 
+		document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit)
+	}
+	// itemAddSubmit function
+	const itemAddSubmit = function(event){
+		const input=UICtrl.getItemInput()
+		if(input.name !== '' && input.calories !== ''){
+			const NewItem = ItemCtrl.addItem(input.name, input.calories)
+			console.log(newItem)
+		}
+
+		event.preventDefault()
+	}
+return {
+init: function(){
+console.log('Initializing App')
+// fetch items from data structure
+const items = ItemCtrl.getItems()
+// popilate items list
+UICtrl.populateItemList(items)
+loadEventListeners()
+}
+}
+})(ItemCtrl, UICtrl);
+
+// Initialize App
+App.init()
